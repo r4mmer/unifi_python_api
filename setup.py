781a62d75ff55c276dfb72485970688391502dad
André Carneiro <acarneiro@mambowifi.com>
2019-11-01 19:00:30 -0300

    change name for pypi

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "unifi_api",
+    name = "unifi_client",
     version = "0.1.0",
     packages = find_packages(),
     install_requires = [
