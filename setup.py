7aa0ea37b83b7c1e241e738ced2d067a9802d3df
André Carneiro <acarneiro@mambowifi.com>
2019-11-01 18:36:48 -0300

    Bump version.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "unifi_api",
-    version = "0.1",
+    version = "0.1.0",
     packages = find_packages(),
     install_requires = [
         'requests>=2.21.0,<3',
